3893c5f14a4b793d068053c8e971ff00f60cdae6
Dmitry Kutakov <vkd.castle@gmail.com>
2020-01-05 12:46:54 +0100

    improve benchmarking

--- Makefile
+++ Makefile
@@ -9,10 +9,21 @@ cover-web:
 clear:
 	@rm cover.prof
 
+bench-old:
+	make BENCH_FILE=bench.old bench-delta
+
+bench-new:
+	make BENCH_FILE=bench.new bench-delta
+
 benchcmp:
-	go test -benchmem -bench=. | tee  bench.new
-	@git stash --quiet
-	go test -benchmem -bench=. | tee  bench.old
-	@git stash pop --quiet
 	benchcmp bench.old bench.new
-	@rm bench.new bench.old
+
+benchstat:
+	benchstat bench.old bench.new
+
+BENCH_FILE=bench.old
+BENCH_COUNT=5
+bench-delta:
+	@rm -f ${BENCH_FILE}
+	for i in {1..${BENCH_COUNT}}; do /bin/echo -n "$$i.."; go test -bench=. >> ${BENCH_FILE}; done
+	@/bin/echo "Done"
--- benchmark_test.go
+++ benchmark_test.go
@@ -7,7 +7,7 @@ import (
 	"github.com/vkd/gowalker"
 )
 
-func BenchmarkWalk(b *testing.B) {
+func BenchmarkWalk_NewWalker(b *testing.B) {
 	var cfg struct {
 		Name string
 		DB   struct {
@@ -57,7 +57,7 @@ func BenchmarkWalk_MapSource(b *testing.B) {
 	}
 }
 
-func BenchmarkWalk_Wrap(b *testing.B) {
+func BenchmarkWalk_NewWalker_ConcatNamer(b *testing.B) {
 	var cfg struct {
 		Name string
 		DB   struct {
@@ -77,30 +77,6 @@ func BenchmarkWalk_Wrap(b *testing.B) {
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
 		err := gowalker.WalkFullname(&cfg, w, gowalker.ConcatNamer)
-		if err != nil {
-			b.Fatalf("Error on walk: %v", err)
-		}
-	}
-}
-
-func BenchmarkWalk_MapSource_Wrap(b *testing.B) {
-	var cfg struct {
-		Name string
-		DB   struct {
-			Type string
-			Port int
-		}
-	}
-
-	env := map[string]string{
-		"NAME":    "service",
-		"DB_TYPE": "postgres",
-		"DB_PORT": "9000",
-	}
-
-	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		err := gowalker.WalkFullname(&cfg, mapSourceConfigWalker(env), gowalker.ConcatNamer)
 		if err != nil {
 			b.Fatalf("Error on walk: %v", err)
 		}
